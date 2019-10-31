38c13d35d3f89efe9a1089e6e404cca3337ed5a2
Anurag <anurag07061@gmail.com>
2019-10-31 19:38:06 +0600

    Prizebond Data

--- server/services/index.js
+++ server/services/index.js
@@ -1,51 +1,19 @@
-const request = require('request');
 const scraper = require('table-scraper');
 
-function extractContent(s, space) {
-    var span= document.createElement('span');
-    span.innerHTML= s;
-    if(space) {
-        var children= span.querySelectorAll('*');
-        for(var i = 0 ; i < children.length ; i++) {
-        if(children[i].textContent)
-            children[i].textContent+= ' ';
-        else
-            children[i].innerText+= ' ';
-        }
-    }
-    return [span.textContent || span.innerText].toString().replace(/ +/g,' ');
-};
-
-let clientServerOptions = {
-    uri: 'https://www.bb.org.bd/investfacility/prizebond/searchPbond.php?txtNumbers=' + '0227049',
-    // body: JSON.stringify(postData),
-    method: 'GET',
-    // headers: {
-    //     'Content-Type': 'application/json'
-    // }
-}
+let uri= 'https://www.bb.org.bd/investfacility/prizebond/searchPbond.php?txtNumbers=';
 
 module.exports = {
     home : function(req, res, next){
         res.render('index', { title: 'Prizebond' });
     },
     prizebonds : function(req, res){
+        //0227046~0227050,0227049
+        //1234708
         scraper
-            .get('https://www.bb.org.bd/investfacility/prizebond/searchPbond.php?txtNumbers=0013308,0013807')
+            .get( uri + '0227046~0227050,0227049')
             .then((tableData)=> {
-                console.log(tableData)
+                tableData.map( data => console.log(data.length))
                 res.send(tableData)
             })
-        // request(clientServerOptions, function (error, response) {
-        //     // str = response.body.replace(/<[^>]+>/g, '');
-        //     // str = str.replace(/\t/g, '');
-        //     // str = str.replace(/\n/g, '');
-        //     // str = str.replace(/\r/g, '');
-        //     // arr = str.trim().split(' ')
-
-        //     // console.log(arr);
-        //     // res.send(arr);
-        //     // res.render('result', { result : html2json(response.body)});
-        // });
     } 
 }
